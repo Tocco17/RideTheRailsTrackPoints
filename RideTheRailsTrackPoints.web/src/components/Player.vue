b55a0ab29c6f478de661f45ad7567046903fdfc1
Tocco17 <fede.ruocco@gmail.com>
2022-09-08 17:53:20 +0200

    Management of Player component behaviour

--- RideTheRailsTrackPoints.web/src/components/Player.vue
+++ RideTheRailsTrackPoints.web/src/components/Player.vue
@@ -1,21 +1,62 @@
 <script>
+import Color from "./Color.vue";
 export default {
-    name: 'player',
+    name: "player",
     props: {
-
+        editable: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        player: {
+            required: true,
+            default: {
+                name: "",
+                colors: ""
+            }
+        },
+        prova: {
+            required: false,
+            default: ''
+        }
     },
     data() {
-
     },
     methods: {
-
+        colorClicked() {
+            this.player.check = !this.player.check
+        },
+        changeName(event) {
+            console.log(event)
+            debugger
+            this.player.name = event.target.value
+        }
     },
     computed: {
-
-    }
+        colorVariable() {
+            return {
+                "--color": this.player.color.RGBA
+            };
+        }
+    },
+    components: { Color }
 }
 </script>
 
 <template>
-
+    <div class="player">
+        <Color :color="player.color" :check="player.check" @color-clicked="colorClicked"></Color>
+        <div v-show="player.check">
+            <input v-if="editable" type="text" v-model="player.name" placeholder="Insert a name" required />
+            <label v-else>{{ player.name }}</label>
+        </div>
+    </div>
 </template>
+
+<style>
+.player {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+}
+</style>
