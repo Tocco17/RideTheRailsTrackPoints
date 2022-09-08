e9b96c7070cdeb01d3cd7d6781519247ee11a4d4
Tocco17 <fede.ruocco@gmail.com>
2022-09-08 17:52:55 +0200

    Added some graphics

---
+++ RideTheRailsTrackPoints.web/src/components/Color.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+	name: 'player',
+	props: {
+		color: {
+			required: true,
+			default: {
+				Name: '',
+				Hex: ''
+			}
+		},
+		radius: {
+			required: false,
+			default: '30px'
+		},
+		check: {
+			type: Boolean,
+			required: true,
+			default: false
+		}
+	},
+	data() {
+	},
+	methods: {
+		clicked() {
+			this.$emit('colorClicked')
+		}
+	},
+	computed: {
+		colorVariable() {
+			return {
+				'--color': this.color.Hex,
+				'--radius': this.radius,
+				'--opacity': this.check ? 1 : 0.5
+			}
+		}
+
+	}
+}
+</script>
+    
+<template>
+	<div class="color" :style="colorVariable" @click="clicked">
+	</div>
+</template>
+    
+<style>
+.color {
+	width: var(--radius);
+	height: var(--radius);
+	background-color: var(--color);
+	border-color: black;
+	border-style: solid;
+	border-width: 1px;
+	border-radius: 100%;
+	opacity: var(--opacity);
+}
+</style>
