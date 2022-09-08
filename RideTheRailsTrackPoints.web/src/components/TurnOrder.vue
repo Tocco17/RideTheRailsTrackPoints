69547732f3bad1e7fd82f5f5e073b849f5cf8dac
Tocco17 <fede.ruocco@gmail.com>
2022-09-08 17:54:23 +0200

    Added turn order view component

---
+++ RideTheRailsTrackPoints.web/src/components/TurnOrder.vue
@@ -0,0 +1,29 @@
+<script>
+import { Colors } from '../classes/Color';
+import Player from './Player.vue';
+
+export default {
+    name: "turnOrder",
+    props: {
+        players: {
+            required: true
+        }
+    },
+    data() {
+    },
+    methods: {
+    },
+    computed: {
+    },
+    components: { Player }
+}
+</script>
+    
+    <template>
+    <h2>Turn order</h2>
+    <ul>
+        <li v-for="player in players" :key="index">
+            <Player :player="player" :editable="false"></Player>
+        </li>
+    </ul>
+</template>
