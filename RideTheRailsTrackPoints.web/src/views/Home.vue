35cf8dc8b45e3c252b63efa0430dff4d21a5756a
Tocco17 <fede.ruocco@gmail.com>
2022-09-09 18:01:42 +0200

    Fake turns, inserted game phases, turn order management

--- RideTheRailsTrackPoints.web/src/views/Home.vue
+++ RideTheRailsTrackPoints.web/src/views/Home.vue
@@ -3,16 +3,24 @@ import { defineComponent } from "vue";
 import NumberOfPlayers from "../components/NumberOfPlayers.vue";
 import { shuffle } from "../utils/Randomize";
 import TurnOrder from "../components/TurnOrder.vue";
+import { Player } from "../classes/Player";
+import { Colors } from "../classes/Color";
+import TakeAShare from "../components/TakeAShare.vue";
+import BuildRailroadTrack from "../components/BuildRailroadTrack.vue";
+import RideTheRails from "../components/RideTheRails.vue";
 
 export default defineComponent({
   data() {
     return {
       gamePhase: 0,
-      players: []
+      players: [],
+      playerTurn: -1,
+      setted: false
     };
   },
-  components: { NumberOfPlayers, TurnOrder },
-  setup() { },
+  components: { NumberOfPlayers, TurnOrder, TakeAShare, BuildRailroadTrack, RideTheRails },
+  setup() {
+  },
   methods: {
     initGame(selectedPlayers) {
       this.players = selectedPlayers.map(p => {
@@ -23,17 +31,44 @@ export default defineComponent({
         }
       })
       this.manageTurnOrder(true)
+      console.log('---------------------')
+      console.log('Initialize the game')
       console.table(this.players)
+      console.log('---------------------')
       this.gamePhase++
+      this.playerTurn++
+      this.playerInTurn.check = true
     },
     manageTurnOrder(isRandom) {
       if (isRandom) shuffle(this.players)
       else this.players.sort((p1, p2) => p1.points > p2.points ? 1 : -1)
 
       this.players.forEach((p, index) => p.turnOrder = index)
+    },
+    createGame() {  //TODO - To delete
+      const playersAux = [
+        new Player('Fede', 'green'),
+        new Player('Leo', 'red'),
+        new Player('Bea', 'white'),
+      ]
+      this.initGame(playersAux)
+    },
+    nextPlayer() {
+      this.playerInTurn.check = false
+      if (this.playerTurn === this.players.length - 1) this.playerTurn = 0
+      else this.playerTurn++
+      this.playerInTurn.check = true
     }
   },
   computed: {
+    setUpGame() {
+      if (this.setted) return
+
+      this.createGame()
+
+      this.setted = true
+      return this.gamePhase
+    },
     isSelectPlayersPhase() {
       return this.gamePhase === 0
     },
@@ -49,22 +84,43 @@ export default defineComponent({
     isRideTheRailsPhase() {
       return this.gamePhase === 3
     },
+    playerInTurn() {
+      return this.players[this.playerTurn]
+    },
+
 
   },
 });
+
 </script>
 
 <template>
   <div class="flex flex-row justify-start items-start">HOME</div>
 
+  <div>Phase {{ setUpGame }}</div>
+
   <div v-if="isSelectPlayersPhase">
     <NumberOfPlayers @init-game="initGame"></NumberOfPlayers>
   </div>
+
   <div v-else>
     <h2>Game</h2>
 
     <div>
       <TurnOrder :players="players"></TurnOrder>
+      <button @click="nextPlayer">Next</button>
+    </div>
+
+    <div v-if="isTakeASharePhase">
+      <TakeAShare :player="playerInTurn"></TakeAShare>
+    </div>
+
+    <div v-if="isBuildRailroadTrackPhase">
+      <BuildRailroadTrack :player="playerInTurn"></BuildRailroadTrack>
+    </div>
+
+    <div v-if="isRideTheRailsPhase">
+      <RideTheRails :player="playerInTurn"></RideTheRails>
     </div>
   </div>
 </template>
