d79b3cc4e459b2e596a9d1057cdfb19fcb249398
Tocco17 <fede.ruocco@gmail.com>
2022-09-09 18:00:42 +0200

    Created game phase components

---
+++ RideTheRailsTrackPoints.web/src/components/RideTheRails.vue
@@ -0,0 +1,22 @@
+<script>
+import Player from './Player.vue';
+
+export default {
+    name: "rideTheRails",
+    props: {
+        player: {
+            required: true
+        }
+    },
+    data() {
+    },
+    methods: {
+    },
+    computed: {
+    },
+    components: { Player }
+}
+</script>
+            
+    <template>
+</template>
---
+++ RideTheRailsTrackPoints.web/src/components/TakeAShare.vue
@@ -0,0 +1,22 @@
+<script>
+import Player from './Player.vue';
+
+export default {
+    name: "takeAShare",
+    props: {
+        player: {
+            required: true
+        }
+    },
+    data() {
+    },
+    methods: {
+    },
+    computed: {
+    },
+    components: { Player }
+}
+</script>
+        
+<template>
+</template>
