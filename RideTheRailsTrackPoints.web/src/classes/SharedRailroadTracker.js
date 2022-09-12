d0d29d6e015200e0ce387eceba2b9dbf8aa64d12
Tocco17 <fede.ruocco@gmail.com>
2022-09-12 08:26:23 +0200

    Created a new class for the playerbard tracker and modify behaviour

--- RideTheRailsTrackPoints.web/src/classes/SharedRailroadTracker.js
+++ RideTheRailsTrackPoints.web/src/classes/SharedRailroadTracker.js
@@ -6,6 +6,11 @@ export class SharedRailroadTracker {
         this.locomotive = Locomotives[name]
         this.maxSharedElements = maxElements
         this.shared = 0
+        this.spaces = []
+
+        for (let i = 0; i < maxElements; i++) {
+            this.spaces.push(new RailroadSpace(i))
+        }
     }
 
     takeAShare() {
@@ -15,5 +20,15 @@ export class SharedRailroadTracker {
         }
 
         this.shared++
+
+            const lastShared = this.spaces.filter(s => s.check).length
+        this.spaces[lastShared].check = true
+    }
+}
+
+class RailroadSpace {
+    constructor(index) {
+        this.index = index
+        this.check = false
     }
 }
