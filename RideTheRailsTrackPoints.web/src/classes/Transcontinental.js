c02ab9bbbfb49472b7e104a47c985ec9cd8b2791
Tocco17 <fede.ruocco@gmail.com>
2022-09-14 12:06:00 +0200

    Added methods to toggle transcontinental goal

--- RideTheRailsTrackPoints.web/src/classes/Transcontinental.js
+++ RideTheRailsTrackPoints.web/src/classes/Transcontinental.js
@@ -8,19 +8,59 @@ export class Transcontinental {
         this.name = 'Transcontinental' //Name
         this.fullPoints = 12 //Maximum points it can give
         this.points = 8 //Minimum points it can give
+        this.check = false
         this.achievedPlayer = null //Player that achived this goal
-        this.takeable = true //If it's still available
+        this.available = true //If it's still available
     }
 
-    // isTakeable() {
-    //     return this.takeable
+    /*
+    Toggle the goal in full mode
+    */
+    toggleFull(player) {
+        this.check ? this.untake(player, this.fullPoints) : this.take(player, this.fullPoints)
+    }
+
+    /*
+    Toggle the goal in normal mode
+    */
+    toggleNormal(player) {
+        this.check ? this.untake(player, this.points) : this.take(player, this.points)
+    }
+
+    /*
+    The goal is taken by the specified player
+    If it's a complete take than it's no more available
+    */
+    take(player, points) {
+        if (!this.available) return
+
+        this.check = true
+        this.achievedPlayer = player
+
+        player.score(points)
+    }
+
+    /*
+    The city becomes available
+    */
+    untake(player, points) {
+        this.check = false
+        this.achievedPlayer = null
+        this.available = true
+        player.untakePoints(points)
+    }
+
+
+
+    // isavailable() {
+    //     return this.available
     // }
 
     // take(player, isFull) {
-    //     if (!this.isTakeable()) return
+    //     if (!this.isavailable()) return
 
     //     this.achievedPlayer = player
-    //     this.takeable = false
+    //     this.available = false
 
     //     const points = isFull ? this.fullPoints : this.points
     //     player.score(points)
