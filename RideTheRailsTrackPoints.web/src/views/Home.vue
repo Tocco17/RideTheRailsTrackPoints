d15a3a7b784cab3276e9f0c3795c965689e18f51
Federico Ruocco <f.ruocco@eustema.it>
2022-10-03 21:06:18 +0200

    Menu style

--- RideTheRailsTrackPoints.web/src/views/Home.vue
+++ RideTheRailsTrackPoints.web/src/views/Home.vue
@@ -3,33 +3,112 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   data() {
+    const firstVoice = document.querySelector('li[data-index="1"]');
+    const position = this.getPosition(firstVoice);
     return {
+      iconX: position.x + "px",
+      iconY: position.y + "px",
     };
   },
-  components: {
-  },
-  setup() { },
+  components: {},
+  setup() {},
   methods: {
+    mouseEnterVoiceMenu(indexVoice) {
+      let voice = document.querySelector(`li[data-index='${indexVoice}']`);
+      voice.classList.add("mouseHover");
+      this.changeIconPosition(voice);
+    },
+    mouseLeaveVoiceMenu(indexVoice) {
+      let voice = document.querySelector(`li[data-index='${indexVoice}']`);
+      voice.classList.remove("mouseHover");
+    },
+    getPosition(voice) {
+      if (!voice)
+        console.log(
+          "Voice undefined, cannot calculate position. Position set to default"
+        );
 
+      const position = voice
+        ? voice.getBoundingClientRect()
+        : { x: 600, y: 600 };
+
+      const x = position.x;
+      const y = position.y;
+      return { x, y };
+    },
+    changeIconPosition(voice) {
+      const position = this.getPosition(voice);
+      this.iconX = position.x + "px";
+      this.iconY = position.y + "px";
+    },
   },
   computed: {
     menuPosition() {
       return {
-        'padding-bottom': '20%'
-      }
-    }
+        "padding-top": "20%",
+      };
+    },
+    trainIconPosition() {
+      // // return;
+      // const activeVoice = document.querySelector("li.mouseHover")
+      //   ? document.querySelector("li.mouseHover")
+      //   : document.querySelector("li[data-index='1']");
+
+      const activeVoice = document.querySelector("li[data-index='1']");
+
+      if (!activeVoice) return;
+
+      const position = activeVoice.getBoundingClientRect();
+      const x = position.x;
+      const y = position.y;
+
+      return {
+        left: x + "px",
+        top: y + "px",
+      };
+    },
   },
 });
 </script>
 
 <template>
   <ul class="menu" :style="menuPosition">
-    <li>PLAY</li>
-    <li>STATISTICS</li>
-    <li>OPTIONS</li>
-    <li>CREDITS</li>
+    <li
+      data-index="1"
+      @mouseenter="mouseEnterVoiceMenu(1)"
+      @mouseleave="mouseLeaveVoiceMenu(1)"
+    >
+      PLAY
+    </li>
+    <li
+      data-index="2"
+      @mouseenter="mouseEnterVoiceMenu(2)"
+      @mouseleave="mouseLeaveVoiceMenu(2)"
+    >
+      STATISTICS
+    </li>
+    <li
+      data-index="3"
+      @mouseenter="mouseEnterVoiceMenu(3)"
+      @mouseleave="mouseLeaveVoiceMenu(3)"
+    >
+      OPTIONS
+    </li>
+    <li
+      data-index="4"
+      @mouseenter="mouseEnterVoiceMenu(4)"
+      @mouseleave="mouseLeaveVoiceMenu(4)"
+    >
+      CREDITS
+    </li>
   </ul>
-
+  <div>
+    <img
+      class="trainIcon"
+      src="../assets/sagomaTrenoWithoutBackground.png"
+      :style="trainIconPosition"
+    />
+  </div>
 </template>
 
 <style>
@@ -38,7 +117,7 @@ body {
   background-repeat: no-repeat;
   background-size: cover 150%;
   background-position: center top;
-  background-attachment: fixed;
+  background-attachment: scroll;
 }
 
 ul.menu {
@@ -49,7 +128,28 @@ ul.menu {
 }
 
 ul.menu li {
-  color: white;
+  color: #faf8d9;
   padding: 0.222rem;
+  text-align: center;
+  font-family: "Calibri (Body)";
+  cursor: pointer;
+}
+
+ul.menu li:not(.mouseHover) {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+ul.menu li.mouseHover {
+  font-size: 40px;
+  font-weight: bolder;
+}
+
+.trainIcon {
+  width: 40px;
+  position: absolute;
+  -webkit-box-reflect: left;
+  left: v-bind(iconX);
+  top: v-bind(iconY);
 }
 </style>
