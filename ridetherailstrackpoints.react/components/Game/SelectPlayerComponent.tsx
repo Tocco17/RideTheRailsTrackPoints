c723b4f763b366ae1bc55746c9e8f79eecb699f8
FedericoRuocco <federico.ruocco@giuneco.it>
2023-02-17 09:35:21 +0100

    Removed MUI

--- ridetherailstrackpoints.react/components/Game/SelectPlayerComponent.tsx
+++ ridetherailstrackpoints.react/components/Game/SelectPlayerComponent.tsx
@@ -1,5 +1,4 @@
 import Player from "@/models/Player";
-import { Slider } from "@mui/material";
 import { useState } from "react";
 
 
@@ -14,7 +13,6 @@ export default function SelectPlayer(onPlayerSelection: any){
     return (
         <>
         <div>
-            <Slider defaultValue={3} step={1} marks min={3} max={5} onChange={changeNumberOfPlayers} value={number}/>
         </div>
         <div>
             SLIDER DIV: {number}
