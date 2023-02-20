91637bde388462df2b8576619407bd78a23a54da
FedericoRuocco <federico.ruocco@giuneco.it>
2023-02-20 17:18:31 +0100

    Reorderd PlayerBoard and added new methods

--- ridetherailstrackpoints.react/models/PlayerBoard.ts
+++ ridetherailstrackpoints.react/models/PlayerBoard.ts
@@ -2,16 +2,44 @@ import { Colors } from "./Color";
 import SharedRailroad from "./SharedRailroad";
 
 export default class PlayerBoard {
-    public SharedRailroads: SharedRailroad[]
+    public sharedRailroads: SharedRailroad[]
 
     constructor(){
-        this.SharedRailroads = [
-            new SharedRailroad(Colors.Locomotive.black, 2),
-            new SharedRailroad(Colors.Locomotive.purple, 3),
-            new SharedRailroad(Colors.Locomotive.yellow, 4),
-            new SharedRailroad(Colors.Locomotive.orange, 5),
-            new SharedRailroad(Colors.Locomotive.blue, 6),
+        this.sharedRailroads = [
             new SharedRailroad(Colors.Locomotive.red, 6),
+            new SharedRailroad(Colors.Locomotive.blue, 6),
+            new SharedRailroad(Colors.Locomotive.orange, 5),
+            new SharedRailroad(Colors.Locomotive.yellow, 4),
+            new SharedRailroad(Colors.Locomotive.purple, 3),
+            new SharedRailroad(Colors.Locomotive.black, 2),
         ]
+    }
+
+    public add(locomotive: string) {
+        let indexChanged = -1
+
+        this.sharedRailroads = this.sharedRailroads.map((s, i) => {
+            if(s.color.name === locomotive || s.taken < s.limitation){ 
+                s.taken++
+                indexChanged = i
+            }
+            return s
+        })
+
+        return indexChanged
+    }
+
+    public remove(locomotive: string) {
+        let indexChanged = -1
+        
+        this.sharedRailroads = this.sharedRailroads.map((s, i) => {
+            if(s.color.name === locomotive || s.taken < s.limitation){ 
+                s.taken--
+                indexChanged = i
+            }
+            return s
+        })
+
+        return indexChanged
     }
 }
