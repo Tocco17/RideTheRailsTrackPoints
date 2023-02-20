0f90990b391ac041c5293ea3ca746f879805bf11
FedericoRuocco <federico.ruocco@giuneco.it>
2023-02-20 16:09:45 +0100

    Added turn order component

---
+++ ridetherailstrackpoints.react/components/Game/TurnOrderComponent.tsx
@@ -0,0 +1,37 @@
+import PlayerInterface from "@/interfaces/PlayerInterface"
+import { useEffect, useState } from "react"
+import ColorComponent from "./ColorComponent"
+import { IoIosArrowDropright } from "react-icons/io";
+
+type TurnOrderComponentType = {
+    players: PlayerInterface[]
+    className?: string
+}
+
+export default function TurnOrderComponent({players, className} : TurnOrderComponentType) {
+    const [number, setNumber] = useState<number>(0)
+
+    useEffect(() => {
+        setNumber(players.length)
+    }, [players])
+
+    return (
+    <>
+    <div className={`flex flex-row justify-evenly content-center place-content-center ${className ?? ''}`}>
+        {
+            players.map((p, i) => {
+                return (
+                    <>
+                    <ColorComponent color={p.color} check={p.check} key={i}/>
+                    {   
+                        i !== number - 1 &&
+                        <IoIosArrowDropright/>
+                    }
+                    </>
+                )
+            })
+        }
+    </div>
+    </>
+    )
+}
