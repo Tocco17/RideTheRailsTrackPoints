1e28d68bd41b8363b99ab16af51f321ffe388f88
FedericoRuocco <federico.ruocco@giuneco.it>
2023-02-23 12:53:15 +0100

    Edit the SpecialStationComponent

--- ridetherailstrackpoints.react/components/Game/SpecialStationComponent.tsx
+++ ridetherailstrackpoints.react/components/Game/SpecialStationComponent.tsx
@@ -1,19 +1,24 @@
 import SpecialStation from "@/models/SpecialStation"
+import SpecialStationChecked from "@/models/SpecialStationChecked"
 
 type SpecialStationComponentType = {
-    station: SpecialStation,
+    station: SpecialStationChecked,
     onClick: (...props: any) => any
 }
 
 export default function SpecialStationComponent({station, onClick}: SpecialStationComponentType) {
+    station.isFull = SpecialStation.prototype.isFull
     return (
         <>
-        <div className="flex flex-col items-center content-center justify-center">
-            <button className="flex flex-col items-center content-center justify-center special-station-button" onClick={onClick}>
-                <label className="cursor-pointer">+ {station.moneys}$</label>
-                <label className="cursor-pointer">{station.name}</label>
-            </button>
-        </div>
+        {
+            !station.isFull() &&
+            <div className="flex flex-col items-center content-center justify-center">
+                <button className="flex flex-col items-center content-center justify-center special-station-button" onClick={onClick}>
+                    <label className="cursor-pointer">+ {station.moneys}$</label>
+                    <label className="cursor-pointer">{station.name}</label>
+                </button>
+            </div>
+        }
         </>
     )
 }
