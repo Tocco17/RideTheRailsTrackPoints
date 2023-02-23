7b53794293a3f7d04de4ee244861c54e1a272a33
FedericoRuocco <federico.ruocco@giuneco.it>
2023-02-23 12:51:59 +0100

    SpecialStationChecked component created

---
+++ ridetherailstrackpoints.react/models/SpecialStationChecked.ts
@@ -0,0 +1,17 @@
+import SpecialStation from "./SpecialStation"
+
+export default class SpecialStationChecked extends SpecialStation {
+    public id: number
+    public check: boolean
+    public playerIndex: number
+
+    constructor(name: string, moneys: number, limitation?: number){
+        super(name, moneys, limitation)
+        this.id = 0
+        this.check = false
+        this.playerIndex = -1
+        this.isFull = SpecialStation.prototype.isFull
+    }
+
+    public static getSpecialStation = (stations: SpecialStationChecked[], name: string) => stations.find(s => s.name === name) ?? new SpecialStationChecked(name, 0)
+}
